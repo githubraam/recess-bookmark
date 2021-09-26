8f8a49ea5691a4c9007a7e2c923177e4ca048f9d
githubraam <41890930+githubraam@users.noreply.github.com>
2021-09-26 19:24:18 +0530

    Minute counting against each recess added

--- src/components/time-bookmark2/Bookmark.js
+++ src/components/time-bookmark2/Bookmark.js
@@ -19,34 +19,45 @@ const getLocalStartId = () =>{
 	}
 }
 
+
+
 const Bookmark = () =>{
-	//const [startTime,setStartTime] = useState('');
 	const [times,setTimes] = useState(parseTimes);
 	const [startTimeId,setStartTimeId] = useState(getLocalStartId);
 	const [delWarningMsg,setDelWarningMsg] = useState(false);
-
+	const [totalMin,setTotalMin] = useState([]);
 
 
 	const startRecess = () =>{
 		
 		const newTimeArray = {
-			id: new Date().getTime().toString(),
-			startTime: new Date().getHours().toString()+'hr ' + new Date().getMinutes().toString() + 'm',
-			endTime: '',
+			id: new Date().getTime(),
+			start:[
+				{hr: new Date().getHours()},
+				{min: new Date().getMinutes()}
+			],
+			end:[
+				{hr: ''},
+				{min: ''}
+			],
 		}
 
+	
+		
 		setTimes([...times,newTimeArray]);
 
 		setStartTimeId(newTimeArray.id);
+	
 			
 	}
 
+	
 
 	const stopRecess = () =>{
 		const update = times.map((time)=>{
 			if (parseInt(time.id) == parseInt(startTimeId)) {
 			
-				return {...time, endTime: new Date().getHours().toString()+'hr ' + new Date().getMinutes().toString() + 'm'}
+				return {...time, end: [{hr:new Date().getHours().toString()},{min:new Date().getMinutes().toString()}]}
 			}
 			else{
 				return time;
@@ -60,9 +71,12 @@ const Bookmark = () =>{
 
 	useEffect(()=>{
 		localStorage.setItem('localTime',JSON.stringify(times));
-
-		localStorage.setItem('lstartId',startTimeId)
+		localStorage.setItem('lstartId',startTimeId);
 	},[times])
+
+
+
+
 
 	const handleDelAll = () =>{
 		setDelWarningMsg(true);
@@ -71,23 +85,52 @@ const Bookmark = () =>{
 	const confirmYes  = () =>{
 		setTimes([]);
 		setDelWarningMsg(false);
+		setStartTimeId('');
 	}
 	const confirmNo = () =>{
 		setDelWarningMsg(false);
 	}
+
+
+
 	return (
 		<div className="container">
 		<h2 className="title">Record your recess</h2>
+
+			<p style={{color: 'white'}}>
+			</p>
 			<div className="btn-group">
 				
 				{ startTimeId=='' ? <button className="btn add" onClick={startRecess}>Start</button> : <button className="btn stop" onClick={stopRecess}>Stop</button>}
 			</div>
 
+			{/* <div className="timeCounter">{parseInt(startSec/60)}<span className="sec">{startSec}s</span></div> */}
+
 			<ul className="timeList">
 			{
 				times.map((time)=>{
+					let minDiff = (time.end[1].min - time.start[1].min);
+					let hoursDiff = 0;
+
+					if (minDiff<59){
+						hoursDiff = (time.end[0].hr - time.start[0].hr)*60
+					}
+
+					let recessMin =  hoursDiff  + (time.end[1].min - time.start[1].min);
+
+
 					return (
-						<li key={time.id}>Start: {time.startTime} :: End: {time.endTime}</li>
+						<>
+						<li	key={time.id} className="totalRecMin">	{recessMin < 0 ? 'Counting total Min' : recessMin+' min'} </li>
+						
+						<li	key={time.id+1} className="timeBreak">
+						Start: {time.start[0].hr+'hr '}  
+						{ time.start[1].min+'min'} :: 
+
+						End: {time.end[0].hr}{time.end[0].hr && 'hr '}
+						{time.end[1].min}{time.end[0].hr && 'min'}
+						</li>
+						</>
 						)
 				})
 				
